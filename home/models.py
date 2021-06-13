59f05c8310486e9abd62d44800f8c5e1b75b462a
Kateryna Bakhchedzhy <k.bakhchedzhy@gmail.com>
2021-06-13 03:57:35 +0300

    edited after linting checking

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    title = models.TextField(max_length=200)
+    link = models.TextField()
+    creation_date = models.DateTimeField()
+    amount_of_votes = models.IntegerField()
+    author_name = models.TextField(max_length=100)
+
+
+class Comments(models.Model):
+
+    author_name = models.TextField(max_length=100)
+    content = models.TextField()
+    creation_date = models.DateTimeField()
+    news = models.ForeignKey("home.News", on_delete=models.CASCADE)
